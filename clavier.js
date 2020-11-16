2fb688bbb6126ceac4be887fbb734fa2a9b9c8fc
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2020-11-16 11:47:57 +0100

    v1.6

--- clavier.js
+++ clavier.js
@@ -175,3 +175,20 @@ const Keyboard = {
 window.addEventListener("DOMContentLoaded", function () {
     Keyboard.init();
 });
+
+const btnToggle = document.querySelector('.btn-toggle');
+
+btnToggle.addEventListener('click',() => {
+    const body = document.body;
+
+    if(body.classList.contains('dark')){
+        body.classList.add('light')
+        body.classList.remove('dark')
+        btnToggle.innerHTML="Go Dark"
+    
+    } else if(body.classList.contains('light')){
+        body.classList.add('dark')
+        body.classList.remove('light')
+        btnToggle.innerHTML ="Go Light"
+    }
+})
