06b6170c57a7393be21a97d3dfccda2526ada8ba
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2020-11-16 09:48:14 +0100

    v1.1

--- clavier.js
+++ clavier.js
@@ -45,9 +45,9 @@ const Keyboard = {
         const fragment = document.createDocumentFragment();
         const keyLayout = [
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-            "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-             "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+            "a", "z", "e", "r", "t", "y", "u", "i", "o", "p",
+            "caps", "q", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+             "m", "w", "x", "c", "v", "b", "n","?", ",", ".",":", "!",
             "space"
         ];
 
